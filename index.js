1277ab45c7bfaf53b60e5df6f15d7d002919a996
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-26 14:38:18 +0000

    feat:working on product controller(deleteProduct)

--- index.js
+++ index.js
@@ -15,15 +15,6 @@ app.use('/api/products', productRoutes)
 
 
 app.delete('/api/products/:id', async(req,res)=>{
-    try{
-        const {id} = req.params
-        const product = await Product.findByIdAndDelete(id)
-        if(!product) return res.status(404).json({message: "Product does not exist"})
-        
-        res.status(200).json({message:"Product deleted successfully"})
-    }catch(error){
-        res.status(500).json({message: error.message})
-    }
 })
 
 connect(process.env.MONGO_URI).then(() => {
