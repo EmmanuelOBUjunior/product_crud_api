138464e5082ab719e0f0586259cb31502f2dfb12
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-26 14:51:08 +0000

    feat:working on product controller(deleteProduct)

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import productRoutes from './routes/products.route.js'
 import 'dotenv/config'
 
 const app = express()
+
+//middleware
 app.use(json())
 app.use(urlencoded({extended:false}))
 
@@ -11,9 +13,11 @@ app.get('/', (req, res)=>{
     res.send('Hello there')
 })
 
+//Routes
 app.use('/api/products', productRoutes)
 
 
+//Database connection
 connect(process.env.MONGO_URI).then(() => {
     console.log("Connected to database")
     app.listen(3000, ()=> console.log("Server running on port 3000"))
