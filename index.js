909aa5a32224ff9fd99ead1a275bfef6eeefc978
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-26 13:28:36 +0000

    feat:working on fetching one product

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/api/products', async(req, res)=>{
 app.get('/api/product/:id', async(req, res)=>{
     try {
         const {id} = req.params
-        const product = await Product.findByID(id)
+        const product = await Product.findById(id)
         res.status(200).json(product)
     } catch (error) {
         res.status(500).json({message:error.message})
