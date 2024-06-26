893dffe93f4f7620ecd21a4e0ba7f85a68e1a8c5
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-26 14:32:39 +0000

    feat:working on product controller(createProducts)

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -11,8 +11,8 @@ export const createProduct = async (req,res)=>{
 }
 export const getProducts = async (req,res)=>{
     try {
-        const product = await Product.create(req.body)
-        res.status(200).json({message: "Product created", product})
+        const products = await Product.find({})
+        res.status(200).json(products)
     } catch (error) {
         res.status(500).json({message: error.message})
     }
