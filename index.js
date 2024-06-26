6e22598b92ed4144e040cfa58a5c5fa9cef14864
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-26 14:32:46 +0000

    feat:working on product controller(createProducts)

--- index.js
+++ index.js
@@ -27,12 +27,7 @@ app.put('/api/products/:id', async(req,res)=>{
 })
 
 app.get('/api/products', async(req, res)=>{
-    try {
-        const products = await Product.find({})
-        res.status(200).json(products)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
+    
 })
 
 app.get('/api/products/:id', async(req, res)=>{
