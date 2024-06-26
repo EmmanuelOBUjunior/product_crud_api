c8e242c62380a6cf1c69208f0eb4c9f4a0fcb9ec
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-26 12:45:46 +0000

    feat:working on adding product api

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-import { connect } from 'mongoose'
-import express, { json } from 'express'
-import Product from './models/product.model'
+const mongoose = require('mongoose')
+const express = require('express')
+const Product  = require('./models/product.model.js')
 require('dotenv').config()
 
 const app = express()
-app.use(json())
+app.use(express.json())
 
 app.get('/', (req, res)=>{
     res.send('Hello there')
 })
 
-app.post('/api/products', async (req,res)=>{
+app.post('/api/products', (req,res)=>{
     const product = await Product
 })
 
-connect(process.env.MONGO_URI).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log("Connected to database")
     app.listen(3000, ()=> console.log("Server running on port 3000"))
 }).catch((err)=> console.log("Failed to connect to database", err))
