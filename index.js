f2725c2c3dcffc1d3f4fbe2641be333692889bea
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-26 13:34:52 +0000

    feat:working on updating a product

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.post('/api/products', async(req,res)=>{
 
 app.put('/api/product/:id', async(req,res)=>{
     try {
-        
+        const {id} = req.params
     } catch (error) {
         res.status(500).json({message: error.message})
     }
