dc1a45c331b2ef62a4811ea4a39d2b226697c923
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-26 12:53:48 +0000

    feat:working on adding product api

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res)=>{
 app.post('/api/products', async(req,res)=>{
     try {
         const product = await Product.create(req.body)
-        res.status(200).json("Product created successfully", product)
+        res.status(200).json(product)
     } catch (error) {
         res.status(500).json({message: error.message})
     }
