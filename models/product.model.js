5482157b5a3986d51f5c072ec45b9e20e3b08253
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-26 11:55:56 +0000

    feat:working on product model

--- models/product.model.js
+++ models/product.model.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 
-const ProductSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true, default: 1 },
-  quantity: { type: Number, required: true, default: 0 },
-  image: { type: String, required: false },
-});
+const ProductSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true, default: 1 },
+    quantity: { type: Number, required: true, default: 0 },
+    image: { type: String, required: false },
+  },
+  {
+    timestamps: true,
+  }
+);
