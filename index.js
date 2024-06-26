83407e41db0e32acafebfe700179c14bb84d158c
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-26 12:51:17 +0000

    feat:working on adding product api

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ app.get('/', (req, res)=>{
 })
 
 app.post('/api/products', async(req,res)=>{
-    const product = await Product
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json("Product created successfully", product)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
 })
 
 connect(process.env.MONGO_URI).then(() => {
