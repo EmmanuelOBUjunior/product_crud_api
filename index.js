ba376dc3747889a8e230c5be04ac6a32daaa49f2
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-26 11:15:10 +0000

    feat:connecting to mongodb

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res)=>{
     res.send('Hello there')
 })
 
-mongoose.connect('mongodb+srv://obujunioremmanuel:JMRtooppjjDtQaYJ@backenddb.ftzfklg.mongodb.net/Product-API?retryWrites=true&w=majority&appName=backendDB').then(() => {
+mongoose.connect('mongodb+srv://obujunioremmanuel:JMRtooppjjDtQaYJ@backenddb.ftzfklg.mongodb.net/ProdcutAPI?retryWrites=true&w=majority&appName=backendDB').then(() => {
     console.log("Connected to database")
     app.listen(3000, ()=> console.log("Server running on port 3000"))
 }).catch(()=> console.log("Failed to connect to database"))
