9f63d9a3a8637f157fcdd0c134c1c2da7aef1c69
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-26 13:52:14 +0000

    feat:working on deleting a product

--- index.js
+++ index.js
@@ -58,6 +58,8 @@ app.delete('/api/product/:id', async(req,res)=>{
         const {id} = req.params
         const product = await Product.findByIdAndDelete(id)
         if(!product) return res.status(404).json({message: "Product does not exist"})
+        
+        res.status(200).json({message:"Product deleted successfully"})
     }catch(error){
         res.status(500).json({message: error.message})
     }
