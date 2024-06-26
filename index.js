411b2416e07b0c00fad86b9dc5094138e9ac1636
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-26 11:38:39 +0000

    feat:setting up dotenv

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res)=>{
     res.send('Hello there')
 })
 
-mongoose.connect('mongodb+srv://obujunioremmanuel:JMRtooppjjDtQaYJ@backenddb.ftzfklg.mongodb.net/ProductAPI?retryWrites=true&w=majority&appName=backendDB').then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log("Connected to database")
     app.listen(3000, ()=> console.log("Server running on port 3000"))
 }).catch((err)=> console.log("Failed to connect to database", err))
