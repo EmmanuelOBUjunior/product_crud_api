b7020156c7297da002a5397b2f8ce95e1ea11435
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-26 14:34:00 +0000

    feat:working on product controller(getProductById)

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -19,5 +19,13 @@ export const getProducts = async (req,res)=>{
 }
 
 export const getProductById = async(req,res)=>{
+    try {
+        const {id} = req.params
+        const product = await Product.findById(id)
 
+        if(!product) return res.status(404).json({message: "Product does not exist"})
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
 }
