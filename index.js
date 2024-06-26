e844a0e429caeaaa2cbb3cd7fdd8abfa60880051
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-26 11:06:25 +0000

    feat:working on index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 
 app.get('/', (req, res)=>{
     res.send('Hello there')
 })
+
 
 
 app.listen(3000, ()=>{
